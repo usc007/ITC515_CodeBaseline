28c11d651ef8cdd21e3254bc1bdcf355b04edf43
deep-kaucha <dpkaucha@gmail.com>
2016-09-09 20:41:32 +1000

    Update IStudent.java

--- src/datamanagement/IStudent.java
+++ src/datamanagement/IStudent.java
@@ -1,18 +1,24 @@
 package datamanagement;
 
+/*Interface IStudent
+*/
 public interface IStudent {
-
+    
     public Integer getID();
-
+    
+    //settin and gettin firstname of student 
     public String getFirstName();
     public void setFirstName(String firstName);
 
+    //settin and gettin firstname of student
     public String getLastName();
     public void setLastName(String lastName);
-
+    
+    //adding unit record of the student 
     public void addUnitRecord( IStudentUnitRecord record );
     public IStudentUnitRecord getUnitRecord( String unitCode );
-
+    
+    //get unit record list of student
     public StudentUnitRecordList getUnitRecords();
 
 }
