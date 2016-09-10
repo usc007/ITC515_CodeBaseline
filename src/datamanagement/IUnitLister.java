62ab3e10607d9fd402a1cfa49ccdcccf54572562
deep-kaucha <dpkaucha@gmail.com>
2016-09-10 20:14:14 +1000

    Update IUnitLister.java

--- src/datamanagement/IUnitLister.java
+++ src/datamanagement/IUnitLister.java
@@ -1,8 +1,11 @@
 package datamanagement;
-
-public interface IUnitLister {
-
+/*interface IUnitLister defined which implement two method
+*/
+public interface IUnitLister 
+{
+    //clear all unit
     public void clearUnits();
+    //method to add the unit
     public void addUnit(IUnit unit);
 
 }
